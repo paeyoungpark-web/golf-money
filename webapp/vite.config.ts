fa557aad8075b9aaa131610ce2e93ad492492bcf
Park paeyoung <paeyoung.park@gmail.com>
2026-02-18 02:51:47 +0900

    Update vite.config.ts

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,3 +1,4 @@
+// webapp/vite.config.ts
 import build from '@hono/vite-build/cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
@@ -5,7 +6,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
-    build(),
+    build({
+      entry: 'src/index.tsx', // 빌드 시 API 서버 코드로 사용할 파일 지정
+    }),
     devServer({
       adapter,
       entry: 'src/index.tsx'
